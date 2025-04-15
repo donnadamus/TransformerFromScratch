954b3fd340930e641d16746004d6caadce74ffed
Marco Donnarumma <126296999+donnadamus@users.noreply.github.com>
2025-04-15 03:09:30 +0200

    implemented mha block

--- model.py
+++ model.py
@@ -2,6 +2,11 @@ import torch
 import torch.nn as nn
 import math
 
+from django.db.models import F
+
+
+# Look to the notes on the paper when something is not clear
+
 class InputEmbeddings(nn.Module):
 
     # d_model is the size of the embedding vector
@@ -71,7 +76,6 @@ class PositionalEncoding(nn.Module):
         x = self.dropout(x)
         return x
 
-
 class LayerNormalization(nn.Module):
     def __init__(self, d_model, eps=1e-6):
         super().__init__()
@@ -103,3 +107,79 @@ class FeedForward(nn.Module):
         x = self.W2(x)
         x = self.dropout(x)
         return x
+
+# For this particular module look at the paper, in particular the figure
+# with each tensor shape
+
+class MultiHeadAttention(nn.Module):
+    # h is the number of heads
+    def __init__(self, d_model, h, dropout):
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        self.dropout = nn.Dropout(dropout)
+
+        # we want to split the embedding in h heads
+        assert d_model % h == 0, "d_model must be divisible by h"
+
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+
+        self.w_o = nn.Linear(d_model, d_model)
+
+    @staticmethod
+    def attention(self, query, key, value, dropout, mask=None):
+        d_k = query.shape[-1]
+
+        # query, key, value have shapes (batch, h, seq_len, d_k)
+
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k) # shape (batch, h, seq_len, seq_len)
+
+        # now we apply the mask
+
+        if mask is not None:
+            attention_scores = attention_scores.masked_fill(mask == 0, -1e9)
+
+        attention_scores = F.softmax(attention_scores, dim=-1)
+
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        attention = attention_scores @ value # shape (batch, h, seq_len, d_k)
+
+        return attention, attention_scores
+
+    def forward(self, x, mask=None):
+        # This next operation is the same as doing x @ w_q
+        # Actually what happens is this:
+        # x @ w_q.T
+        # but since w_q was stored as (out_features, in_features), so in reverse order
+        # doing self.w_q(x) is effectively the same as x @ w_q
+
+        query = self.w_q(x) # (batch, seq_len, d_model)
+        key = self.w_k(x) # (batch, seq_len, d_model)
+        value = self.w_v(x) # (batch, seq_len, d_model)
+
+        # query is (batch, seq_len, d_model)
+        # query.view makes it (batch, seq_len, h, d_k)
+        # transpose make the final shape (batch, h, seq_len, d_k)
+
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        x, attention_scores = MultiHeadAttention.attention(query, key, value, mask, self.dropout)
+
+        # Combine all the heads together
+        # (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
+        # -1 lets PyTorch infer seq_len automatically (same as x.shape[1])
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        # Multiply by Wo
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        return self.w_o(x)
+
+
+
